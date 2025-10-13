a4667b6b210445e6dbdaba125d40afcf8b459de2
Gary García <Gary.Garcia2703@gmail.com>
2025-10-13 10:54:12 -0300

    upd app.jsx

---
+++ projects/00-Hola-mundo/src/app.jsx
@@ -0,0 +1,17 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="img/mii.png" alt="mii avatar"/>
+                <div>
+                    <strong>Miguel Ángel</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
--- projects/00-Hola-mundo/src/main.jsx
+++ projects/00-Hola-mundo/src/main.jsx
@@ -1,18 +1,11 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {App} from './App.jsx'
+import './index.css'
 
 const conteiner = document.querySelector("div#conteiner")
 const root = createRoot(conteiner)
 
-const elementos = (
-  <StrictMode>
-    <h1>Olá</h1>
-    <button>clicka aqui para dar like</button>
-    <button>aqui para dar dislike</button>
-  </StrictMode>
-  
-)
-
 root.render(
-  elementos
+  <App/>
 )
