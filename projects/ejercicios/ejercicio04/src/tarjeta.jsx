97b2626049201c9a68f0ce4a823c61315a7cecfe
Gary García <Gary.Garcia2703@gmail.com>
2025-10-20 10:46:26 -0300

    upd_tarjeta.jsx

---
+++ projects/ejercicios/ejercicio04/src/tarjeta.jsx
@@ -0,0 +1,12 @@
+import { App } from "./App"
+export function Tarjeta({nombre , titulo , descipcion}) {
+    return (
+        <>
+        <h1>{nombre} </h1>
+
+        <h2>{titulo} </h2>
+        
+        <p>{descipcion} </p>
+        </>
+    )
+}
