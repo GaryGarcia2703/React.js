98319f8cece16e2effe81f5063bfd8fe8a592b3e
Gary García <Gary.Garcia2703@gmail.com>
2025-10-24 21:42:41 -0300

    upd_img

---
+++ projects/ejercicios/ejercicio04/src/objeto.jsx
@@ -0,0 +1,59 @@
+const sonic = {
+    nombre: "Sonic",
+    titulo: "Fastest Thing Alive",
+    descripcion: "Sonic is a legendary blue hedgehog with unmatched speed and reflexes, capable of feats that seem impossible—even challenging and defeating powerful gods in his adventures. Fearless, rebellious, and driven by his own sense of justice, Sonic is a hero in his own way, fighting not for glory, but to protect those he loves. His friends are his greatest treasure, and his chosen family is the heart of his world. Whether racing across cities, forests, or mountains, Sonic proves that true heroism lies in courage, loyalty, and the determination to stand for what is right.",
+
+    titulosJuegos: ["Sonic Unleashed", "Sonic Colours", "Sonic Adventure 2"],
+
+    imagenTarjeta: "img/sonic-nuh.gif",
+    icon: "img/sonic-3.png",
+    imgGameplay:"img/sweet-mountain-p.jpeg", 
+    imgGameplay1:"img/sonic-unleashed-p.jpg",
+    imgGameplay2:"img/sonic-gameplay.png",
+
+    juegoDescripcion1:"In Sonic Colours, Sonic ventures into space to stop Dr. Eggman’s interstellar amusement park and free the alien Wisps from captivity. Armed with their incredible energy, Sonic gains new abilities that push his speed and creativity to new limits. This adventure highlights his optimism and wit — even in the darkest corners of space, Sonic’s confidence and compassion shine brighter than any star.",
+
+    juegoDescripcion2:"In Sonic Unleashed, Sonic faces one of his greatest challenges yet — himself. After being transformed into a “Werehog” by Dark Gaia’s power, Sonic is forced to confront the darkness within while racing to restore the shattered world. By day, he blazes through continents at supersonic speed, and by night, he fights with raw strength and determination. This journey reveals not just Sonic’s unmatched speed, but his heart — proving that even when the world is broken, his spirit never slows down.",
+
+    juegoDescripcion3:"Sonic Adventure 2 tells the story of two opposing forces — Sonic and Shadow — bound by fate and a common desire for justice. Chased by the military and misunderstood by the world, Sonic stands tall, proving that heroes are defined not by how they’re seen, but by what they stand for. His rivalry with Shadow pushes him to his limits, revealing his true strength: a hero who fights not for fame or power, but for freedom, friendship, and the hope of a better tomorrow.",
+}
+
+const shadow = {
+    nombre: "Shadow",
+    titulo: "The Ultimate Life Form",
+    descripcion: "Shadow the Hedgehog was created over fifty years ago on the Space Colony ARK by Professor Gerald Robotnik as the “Ultimate Life Form.” Designed to harness incredible power and combat any threat, he was engineered with advanced abilities far beyond ordinary hedgehogs. Shadow’s existence was deeply tied to Maria Robotnik, whose kindness gave him purpose and a sense of compassion. However, after a tragic incident on the ARK, Shadow was sealed away in stasis, only to reemerge decades later into a world very different from the one he once knew, struggling to reconcile his mission, memories, and identity.",
+
+    titulosJuegos: ["Adventure 2", "Shadow The Hedgehog 2005", "Generations"],
+
+    imagenTarjeta: "img/shadow_foda-removebg-preview.pngf",
+    icon: "img/shadowpeque.png",
+    imgGameplay:"img/Shadow-S2.jpeg", 
+    imgGameplay1:"img/shadow-gameplay.jpeg",
+    imgGameplay2:"img/Shadow_sonic.jpeg",
+
+    juegoDescripcion1:"In Sonic Adventure 2, Shadow the Hedgehog makes his dramatic debut as the “Ultimate Life Form.” Created aboard the Space Colony ARK, his heart is torn between vengeance and the memory of Maria, the only person who ever showed him true kindness. Manipulated by the military and haunted by loss, Shadow begins as a weapon of destruction—but through his bond with Sonic, he rediscovers purpose. In his final act of redemption, he sacrifices himself to save the world, fulfilling Maria’s wish for peace.",
+
+    juegoDescripcion2:"In Shadow the Hedgehog (2005), his past returns to haunt him. Confused by fragmented memories and whispers of his true origin, Shadow embarks on a journey to uncover who he really is—hero, villain, or something in between. Caught between humanity, aliens, and his own conscience, he forges his own path, rejecting manipulation and choosing his destiny. By confronting Black Doom and destroying the truth of his creation, Shadow finally claims his identity—not as a copy or weapon, but as himself.",
+
+    juegoDescripcion3:"In Shadow Generations, Shadow returns as the protagonist in a parallel timeline to his past. Released in October 2024, he must journey through his own memories and face his archenemy Black Doom to prevent a catastrophic time-collapse. Haunted by the loss of Maria and manipulated for years, Shadow now wields his legacy as a weapon—not for destruction, but for redemption. With his signature Chaos Control and newly mastered powers, he steps into the light of his own destiny and proves that the Ultimate Life Form is not defined by his past—he defines his future.",
+}
+
+const silver = {
+    nombre: "silver",
+    titulo: "The Hope Of The Future",
+    descripcion: "Silver is a silver hedgehog from a devastated future, possessing powerful psychokinetic abilities that allow him to move objects, slow time in brief moments, and fight with his mind. Driven by the determination to prevent the catastrophe that destroyed his era, Silver is both idealistic and burdened by pain—his fight is not just for survival, but to give his time a chance at a brighter tomorrow. In the decisive confrontation against Solaris, a temporal entity of apocalyptic power, Silver used his full abilities in a strategic act of sacrifice: he isolated and contained the time streams that fueled Solaris, breaking its connection to time and destroying its energy core. By doing so, he prevented the annihilation of the future, though the victory left deep scars on both Silver and the timeline he fought so hard to save.",
+
+    titulosJuegos: ["Sonic 06", "Generations", "Forces"],
+
+    imagenTarjeta: "img/shadow_foda-removebg-preview.pngf",
+    icon: "img/shadowpeque.png",
+    imgGameplay:"img/Shadow-S2.jpeg", 
+    imgGameplay1:"img/shadow-gameplay.jpeg",
+    imgGameplay2:"img/Shadow_sonic.jpeg",
+
+    juegoDescripcion1:"In Sonic Adventure 2, Shadow the Hedgehog makes his dramatic debut as the “Ultimate Life Form.” Created aboard the Space Colony ARK, his heart is torn between vengeance and the memory of Maria, the only person who ever showed him true kindness. Manipulated by the military and haunted by loss, Shadow begins as a weapon of destruction—but through his bond with Sonic, he rediscovers purpose. In his final act of redemption, he sacrifices himself to save the world, fulfilling Maria’s wish for peace.",
+
+    juegoDescripcion2:"In Shadow the Hedgehog (2005), his past returns to haunt him. Confused by fragmented memories and whispers of his true origin, Shadow embarks on a journey to uncover who he really is—hero, villain, or something in between. Caught between humanity, aliens, and his own conscience, he forges his own path, rejecting manipulation and choosing his destiny. By confronting Black Doom and destroying the truth of his creation, Shadow finally claims his identity—not as a copy or weapon, but as himself.",
+
+    juegoDescripcion3:"In Shadow Generations, Shadow returns as the protagonist in a parallel timeline to his past. Released in October 2024, he must journey through his own memories and face his archenemy Black Doom to prevent a catastrophic time-collapse. Haunted by the loss of Maria and manipulated for years, Shadow now wields his legacy as a weapon—not for destruction, but for redemption. With his signature Chaos Control and newly mastered powers, he steps into the light of his own destiny and proves that the Ultimate Life Form is not defined by his past—he defines his future.",
+}
